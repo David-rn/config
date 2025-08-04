391c552dc715c2216bc0de2ca07b58b464c1879f
David Redo <dredonieto@gmail.com>
2025-08-04 16:06:32 +0200

    Added wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,15 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 17
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+
+config.color_scheme = "tokyonight"
+
+return config
